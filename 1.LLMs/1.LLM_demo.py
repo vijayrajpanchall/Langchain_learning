9d5d38645d2d7efd35e7c4a4a36c99ac1e74e719
vijayrajpanchall <vijayrajpanchal4.com>
2025-05-08 11:18:44 +0530

    first commit

---
+++ 1.LLMs/1.LLM_demo.py
@@ -0,0 +1,9 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(
+    model="gpt-3.5-turbo-instruct")
+response = llm.invoke("What is the capital of France?")
+print(response)
