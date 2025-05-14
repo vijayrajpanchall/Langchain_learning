7a2930b1b783b8aaf5b7bb1a8a468304432b7559
vijayrajpanchall <vijayrajpanchal4.com>
2025-05-14 14:26:41 +0530

    learned about conditional chains

---
+++ Chains/conditional_chain.py
@@ -0,0 +1,55 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="microsoft/Phi-3-mini-4k-instruct",
+    task="text-generation",
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+    sentiment: Literal["positive", "negative"] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template="Classify the sentiment of the following feedback text into positive or negative \n {feedback} \n {format_instruction}",
+    input_variables=['feedback'],
+    partial_variables={'format_instruction': parser2.get_format_instructions()}
+)
+
+
+prompt2 = PromptTemplate(
+    template="Write a appropriate response to this positive feedback \n {feedback}",
+    input_variables=['feedback'],
+)
+
+prompt3 = PromptTemplate(
+    template="Write a appropriate response to this negative feedback \n {feedback}",
+    input_variables=['feedback'],
+)
+
+classifier_chain = prompt1 | model | parser2
+
+branch_chain = RunnableBranch(
+    (lambda x: x.sentiment == "positive", prompt2 | model | parser),
+    (lambda x: x.sentiment == "negative", prompt3 | model | parser),
+    RunnableLambda(lambda x: "No response needed")
+)
+
+chain  = classifier_chain | branch_chain
+
+print(chain.invoke({"feedback": "This is a great smartphone!"}))
