f17cbf990e6b04500959fc74363020418bcd9ee9
vijayrajpanchall <vijayrajpanchal4.com>
2025-05-16 10:03:18 +0530

    learned about runnables

---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,35 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=['topic'],
+)
+
+prompt2 = PromptTemplate(
+    template="Explain the following joke - {text}",
+    input_variables=['text'],
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(
+    prompt1 | model | parser 
+)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2 | model | parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({"topic": "Cricket"})
+print("Result: ", result)
---
+++ Runnables/runnable_sequence.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=['topic'],
+)
+
+prompt2 = PromptTemplate(
+    template="Explain the following joke - {text}",
+    input_variables=['text'],
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(
+    prompt1 | model | parser | prompt2 | model | parser
+)
+
+result = chain.invoke({"topic": "AI"})
+print("Result: ", result)
+print("Graph: ", chain.get_graph().print_ascii())
